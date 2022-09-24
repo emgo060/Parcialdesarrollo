7f131a768396cfef80cfe5002902dc4f0cb666c5
emgo060 <114277490+emgo060@users.noreply.github.com>
2022-09-23 23:37:37 -0600

    Add files via upload

---
+++ inventario-basico/inc/container.php
@@ -0,0 +1,14 @@
+</head>
+
+<body class="bg-gradient bg-dark bg-opacity-25">
+  <nav role="navigation" class="navbar navbar-dark navbar-static-top bg-info bg-gradient">
+    <div class="container">
+      <div class="navbar-header">
+        <a target="_blank" class="navbar-brand">Inventario Parcial Desarrollo
+    
+        </a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
